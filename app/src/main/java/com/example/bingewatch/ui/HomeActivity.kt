95684015e8437d3a68abc1d54c6d301c00e1c63b
Arpan <arpan.peter@reddoorz.com>
2024-04-09 15:33:01 +0530

    Bottom NavBar and Fragments connected with login and splash screens

--- app/src/main/java/com/example/bingewatch/ui/HomeActivity.kt
+++ app/src/main/java/com/example/bingewatch/ui/HomeActivity.kt
@@ -2,15 +2,21 @@ package com.example.bingewatch.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.newsprojectpractice.R
+
 import com.example.newsprojectpractice.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
+        val navController = Navigation.findNavController(this, R.id.moviesNavHostFragment)
+        val bottomNavigation = binding.bottomNavigationView
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
