fab3bf05d039f016bab518fddb80751c0ba71f0a
Arpan <arpan.peter@reddoorz.com>
2024-04-10 17:55:18 +0530

    DetailsActivity.kt added

--- app/src/main/java/com/example/bingewatch/ui/fragments/MoviesFragment.kt
+++ app/src/main/java/com/example/bingewatch/ui/fragments/MoviesFragment.kt
@@ -1,5 +1,6 @@
 package com.example.bingewatch.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,6 +12,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bingewatch.adapters.MovieAdapter
 import com.example.bingewatch.ViewModel.MoviesViewModel
+import com.example.bingewatch.models.Movie
+import com.example.bingewatch.ui.DetailsActivity
+//import com.example.bingewatch.ui.MOVIE_BACKDROP
+//import com.example.bingewatch.ui.MOVIE_OVERVIEW
+//import com.example.bingewatch.ui.MOVIE_POSTER
+//import com.example.bingewatch.ui.MOVIE_RATING
+//import com.example.bingewatch.ui.MOVIE_RELEASE_DATE
+//import com.example.bingewatch.ui.MOVIE_TITLE
 import com.example.newsprojectpractice.R
 
 class MoviesFragment : Fragment() {
@@ -36,4 +45,15 @@ class MoviesFragment : Fragment() {
 
         return view
     }
+    private fun showMovieDetails(movie: Movie) {
+        val intent = Intent(requireContext(), DetailsActivity::class.java)
+        intent.putExtra("MOVIE_BACKDROP", movie.backdropPath)
+        intent.putExtra("MOVIE_POSTER", movie.posterPath)
+        intent.putExtra("MOVIE_TITLE", movie.title)
+        intent.putExtra("MOVIE_RATING", movie.rating)
+        intent.putExtra("MOVIE_RELEASE_DATE", movie.releaseDate)
+        intent.putExtra("MOVIE_OVERVIEW", movie.overview)
+        startActivity(intent)
+    }
+
 }
